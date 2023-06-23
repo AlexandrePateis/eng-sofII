c0657e728636019f70a664804963f435bbea7361
Alexandre Pateis <alepateis9090@gmail.com>
2023-06-23 00:41:46 -0300

    ransack implementado

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,7 @@
 class TransactionsController < ApplicationController
   def extrato
-    @transactions = current_user.transactions.order(created_at: :desc).page(params[:page]).per(15)
-    @transfer_transactions = current_user.transfer_transactions.page(params[:page]).per(15)
+    @ransack_search = current_user.transactions.ransack(params[:q])
+    @transactions = @ransack_search.result.order(created_at: :desc)
   end
+  
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -6,4 +6,7 @@ class Transaction < ApplicationRecord
 
     TRANSACTION_TYPES = ['Depósito', 'Saque', 'Transferência']
 
+    def self.ransackable_attributes(auth_object = nil)
+        %w[account_id action amount created_at description id source_account_id target_account_id transaction_type updated_at user_id]
+      end
 end
